1bc6adf0ed7e9d4b020ebf5fbc13f9b5d077f280
stephanusnicolaus <stevnico93@gmail.com>
2018-11-06 21:12:21 +0700

    change comment

--- src/scenes/Home/Home.test.js
+++ src/scenes/Home/Home.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Home from './index'
 
 describe('Test Home lho', () => {
-    it('it render successfully', () => {})
-    it('ada text di dalamnya', () => {})
-    it('props title sesuai', () => {})
+    // it('it render successfully', () => {})
+    // it('ada text di dalamnya', () => {})
+    // it('props title sesuai', () => {})
 })
